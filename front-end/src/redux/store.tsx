2e02c16027f0265dd7d678ef1b36ef00b49eff80
Precious <107755650+PreciousAbe@users.noreply.github.com>
2023-03-11 10:09:08 +0100

    first commit

---
+++ front-end/src/redux/store.tsx
@@ -0,0 +1,9 @@
+import { legacy_createStore as createStore, applyMiddleware, compose } from "redux"
+import thunk from "redux-thunk"
+import rootReducer from "./reducers/rootReducer"
+const composeEnhancers =
+  (window["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] as typeof compose) || compose
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+)
